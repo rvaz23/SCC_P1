3d5aff9f0d7c8adb20ee994f5da7948e41a946d8
Sergio <sergio.seco@leanbakery.com>
2021-10-12 12:54:18 +0100

    Draft da List Function

--- src/scc/utils/UploadToStorage.java
+++ src/scc/utils/UploadToStorage.java
@@ -5,6 +5,7 @@ import com.azure.core.util.BinaryData;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobContainerClientBuilder;
+import com.azure.storage.blob.models.BlobItem;
 
 
 public class UploadToStorage {
@@ -17,7 +18,7 @@ public class UploadToStorage {
 		
 
 		// Get connection string in the storage access keys page
-		String storageConnectionString = "TO BE COMPLETED";
+		String storageConnectionString = "ZVZ2knY7OT+3hSmDnmx2B5aeN1dmPlUh8AYtIwx5tj7oLD7LvIbFgxvomCppmropkOhZl539nwsMk3POMRW4jg==";
 
 		try {
 			BinaryData data = BinaryData.fromFile(Path.of(filename));
@@ -31,13 +32,36 @@ public class UploadToStorage {
 			// Get client to blob
 			BlobClient blob = containerClient.getBlobClient( filename);
 
+            for (BlobItem blobItem : containerClient.listBlobs()) {
+                System.out.println("\t" + blobItem.getName());
+            }
+
 			// Upload contents from BinaryData (check documentation for other alternatives)
 			blob.upload(data);
 			
 			System.out.println( "File updloaded : " + filename);
-			
+
+
 		} catch( Exception e) {
 			e.printStackTrace();
 		}
 	}
+
+
+    public void list(String filename){
+        // Get connection string in the storage access keys page
+        String storageConnectionString = "ZVZ2knY7OT+3hSmDnmx2B5aeN1dmPlUh8AYtIwx5tj7oLD7LvIbFgxvomCppmropkOhZl539nwsMk3POMRW4jg==";
+
+        BlobContainerClient containerClient = new BlobContainerClientBuilder()
+                .connectionString(storageConnectionString)
+                .containerName("images")
+                .buildClient();
+
+        BlobClient blob = containerClient.getBlobClient( filename);
+
+        for (BlobItem blobItem : containerClient.listBlobs()) {
+            System.out.println("\t" + blobItem.getName());
+        }
+
+    }
 }
