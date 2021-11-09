181251b87af67f7f3d1e21a32f6906ba2106060c
rvaz23 <rvaz23@gmail.com>
2021-11-09 12:01:08 +0000

    Redis Credentials

--- src/scc/cache/RedisCache.java
+++ src/scc/cache/RedisCache.java
@@ -13,8 +13,8 @@ import redis.clients.jedis.JedisPoolConfig;
 import scc.data.User;
 
 public class RedisCache {
-	private static final String RedisHostname = "scc2122cache.redis.cache.windows.net";
-	private static final String RedisKey = "pfPB5YXVKERrWCNlIj15CyXvTWAnpo8BVAzCaGVgyH0=";
+	private static final String RedisHostname = "lab152656.redis.cache.windows.net";
+	private static final String RedisKey = "3cTDiucqBEL39BHdJZyyW44mqDtP6IUZFAzCaMWf8PM=";
 	
 	private static RedisCache instance;
 	
